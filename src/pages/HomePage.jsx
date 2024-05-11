6558fd27838def672ca5bbf4f58f5dd8a95b6ba1
icechris <duoduchristian36@gmail.com>
2024-05-11 18:51:55 +0000

    created a backend using json server to be able to make requests

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero';
 import HomeCards from '../components/HomeCards';
 import JobListings from '../components/JobListings';
+import ViewButton from '../ViewButton';
 
 
 const homePage = () => {
@@ -10,6 +11,7 @@ const homePage = () => {
       <Hero />
       <HomeCards  />
       <JobListings  isHome = {true}/>
+      <ViewButton />
 
     </div>
   )
